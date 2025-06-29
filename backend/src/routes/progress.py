f4cf3213f0b297808d1eac5ba12157558ea4d84f
ryu-ryuk <meow@alokranjan.me>
2025-06-30 03:50:50 +0530

    progress route

--- backend/src/routes/progress.py
+++ backend/src/routes/progress.py
@@ -4,15 +4,26 @@ handles the /progress endpoint.
 - fetches user’s topic-wise performance
 - returns mastery level, improvement trends, and last interaction
 """
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from services.tracker import get_user_progress_from_pathway
 from models import ProgressResponse
 from services.event_logger import log_user_event
+from services.tracker import get_user_progress_from_db
 
 router = APIRouter()
 
+# @router.get("/progress", response_model=ProgressResponse)
+# async def get_progress(user_id: str):
+#     log_user_event(user_id, "progress_view", None)
+#     progress = get_user_progress_from_pathway(user_id)
+#     return progress
+
+##################################################
+
 @router.get("/progress", response_model=ProgressResponse)
 async def get_progress(user_id: str):
-    log_user_event(user_id, "progress_view", None)
-    progress = get_user_progress_from_pathway(user_id)
-    return progress
+    data = get_user_progress_from_db(user_id)
+    if data["topics"] == []:
+        raise HTTPException(status_code=404, detail="No progress found")
+        pass
+    return data
