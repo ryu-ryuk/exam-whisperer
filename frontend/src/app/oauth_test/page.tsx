df3c9a911fa47b3c1929677a29cc91cb2762149f
Arjithar8 <arjithar8@gmail.com>
2025-07-02 01:23:02 +0530

    Test OAuth partial

--- frontend/src/app/oauth_test/page.tsx
+++ frontend/src/app/oauth_test/page.tsx
@@ -1,11 +1,14 @@
-import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleLogin } from '@react-oauth/google';
 
 export default function OAuthTestPage() {
     return (
-        <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || ""}>
-            <div className="flex h-screen items-center justify-center">
-                <h1 className="text-2xl font-bold">OAuth Test Page</h1>
-            </div>
-        </GoogleOAuthProvider>
+        <GoogleLogin
+        onSuccess={credentialResponse => {
+            console.log(credentialResponse);
+        }}
+        onError={() => {
+            console.log('Login Failed');
+        }}
+    />
     );
 }
