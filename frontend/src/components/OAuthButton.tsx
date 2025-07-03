1c484ac5659d3a771d832b45f411ce577ca7b4a6
ryu-ryuk <meow@alokranjan.me>
2025-07-03 21:29:18 +0530

    o auth bttn

--- frontend/src/components/OAuthButton.tsx
+++ frontend/src/components/OAuthButton.tsx
@@ -4,20 +4,20 @@ import { GoogleLogin } from '@react-oauth/google';
 export default function OAuthButton() {
     return (
         <GoogleLogin
-        onSuccess={credentialResponse => {
-            const access_token = credentialResponse.credential;
-            fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/google`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ token: access_token }),
-            })
-            // Need to save it
-        }}
-        onError={() => {
-            console.log('Login Failed');
-        }}
-    />
+            onSuccess={credentialResponse => {
+                const access_token = credentialResponse.credential;
+                fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/oauth/google`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ token: access_token }),
+                })
+                // Need to save it
+            }}
+            onError={() => {
+                console.log('Login Failed');
+            }}
+        />
     );
 }
